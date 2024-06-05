bb3ea386c94ef4d801866611f06a274d4473c4f3
artisan24fullstack <denisartisanfullstack@gmail.com>
2024-06-05 12:29:31 +0200

    delete user

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -85,3 +85,47 @@ export const PATCH = async (request: Request) => {
 
   }
 }
+
+
+export const DELETE = async (request: Request) => {
+  try {
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get('userId');
+
+    if (!userId) {
+      return new NextResponse(
+        JSON.stringify({ message: "ID user not found" }),
+        { status: 400 }
+      );
+    }
+
+    if (!Types.ObjectId.isValid(userId)) {
+      return new NextResponse(
+        JSON.stringify({ message: "Invalid User id" }),
+        { status: 400 }
+      );
+    }
+
+    await connect();
+
+    const deleteUser = await User.findByIdAndDelete(
+      new Types.ObjectId(userId)
+    );
+
+    if (!deleteUser) {
+      return new NextResponse(
+        JSON.stringify({ message: "User not found in the database" }),
+        { status: 400 }
+      );
+    }
+    return new NextResponse(
+      JSON.stringify({ message: "User is deleted", user: deleteUser }),
+      { status: 200 }
+    );
+
+
+  } catch (error: any) {
+    return new NextResponse("Error in deleting user" + error.message, { status: 500 });
+
+  }
+}
