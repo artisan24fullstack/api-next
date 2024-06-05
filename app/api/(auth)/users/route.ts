3b716b6e6d5617d6066e1f8752fa8bf05cd2cc83
artisan24fullstack <denisartisanfullstack@gmail.com>
2024-06-05 11:43:01 +0200

    post create user

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -14,3 +14,20 @@ export const GET = async () => {
   }
   //return new NextResponse("This is a api with next");
 };
+
+export const POST = async (request: Request) => {
+  try {
+    const body = await request.json();
+    await connect();
+    const newUser = new User(body);
+    await newUser.save();
+
+    return new NextResponse(JSON.stringify({ message: "User is created", user: newUser }),
+      { status: 200 }
+    );
+
+  } catch (error: any) {
+    return new NextResponse("Error in creating user" + error.message, { status: 500 });
+
+  }
+}
